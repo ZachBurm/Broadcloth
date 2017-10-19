010b60b726d92f5f9b3ea68c0618b0ee37db50da
Zach Burmeister <zachburm@hotmail.com>
2017-10-18 23:55:13 -0600

    F this

--- BroadclothSolution/Build DW/BuildBroadclothDW.sql
+++ BroadclothSolution/Build DW/BuildBroadclothDW.sql
@@ -134,15 +134,39 @@ CREATE TABLE DimCustomer
 --
 -- Table 5 DimDate
 --
-CREATE TABLE DimDate
-	(Date_SK INT IDENTITY (1,1) CONSTRAINT pk_Date_SK PRIMARY KEY,
-	Year NVARCHAR(40) NOT NULL,
-	Quarter NVARCHAR(120) NOT NULL,
-	Month NVARCHAR(20) NOT NULL,
-	Day_of_week NVARCHAR(20) NOT NULL,
-	Day_of_month NVARCHAR(2) NOT NULL,
-	Day_of_year NVARCHAR(3) NOT NULL,
-	Season NVARCHAR(20) NOT NULL
+CREATE TABLE dbo.DimDate
+	(Date_SK INT PRIMARY KEY, 
+	Date DATETIME,
+	FullDate CHAR(10),-- Date in MM-dd-yyyy format
+	DayOfMonth INT, -- Field will hold day number of Month
+	DayName VARCHAR(9), -- Contains name of the day, Sunday, Monday 
+	DayOfWeek INT,-- First Day Sunday=1 and Saturday=7
+	DayOfWeekInMonth INT, -- 1st Monday or 2nd Monday in Month
+	DayOfWeekInYear INT,
+	DayOfQuarter INT,
+	DayOfYear INT,
+	WeekOfMonth INT,-- Week Number of Month 
+	WeekOfQuarter INT, -- Week Number of the Quarter
+	WeekOfYear INT,-- Week Number of the Year
+	Month INT, -- Number of the Month 1 to 12{}
+	MonthName VARCHAR(9),-- January, February etc
+	MonthOfQuarter INT,-- Month Number belongs to Quarter
+	Quarter CHAR(2),
+	QuarterName VARCHAR(9),-- First,Second..
+	Year INT,-- Year value of Date stored in Row
+	YearName CHAR(7), -- CY 2015,CY 2016
+	MonthYear CHAR(10), -- Jan-2016,Feb-2016
+	MMYYYY INT,
+	FirstDayOfMonth DATE,
+	LastDayOfMonth DATE,
+	FirstDayOfQuarter DATE,
+	LastDayOfQuarter DATE,
+	FirstDayOfYear DATE,
+	LastDayOfYear DATE,
+	IsHoliday BIT,-- Flag 1=National Holiday, 0-No National Holiday
+	IsWeekday BIT,-- 0=Week End ,1=Week Day
+	Holiday VARCHAR(50),--Name of Holiday in US
+	Season VARCHAR(10)--Name of Season
 	);
 
 --
